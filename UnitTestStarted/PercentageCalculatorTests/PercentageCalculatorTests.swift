8769a6f2a8821de3bf91be6ebd16318fb439c0dd
张强 <zq252125@163.com>
2016-05-23 14:42:40 +0800

    add UnitTest Demo

---
+++ UnitTestStarted/PercentageCalculatorTests/PercentageCalculatorTests.swift
@@ -0,0 +1,53 @@
+//
+//  PercentageCalculatorTests.swift
+//  PercentageCalculatorTests
+//
+//  Created by Maxime Defauw on 03/02/16.
+//  Copyright © 2016 App Coda. All rights reserved.
+//
+
+import XCTest
+@testable import PercentageCalculator
+
+class PercentageCalculatorTests: XCTestCase {
+    
+    // Unit Tset For ViewController
+    var vc: ViewController!
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: NSBundle.mainBundle())
+        vc = storyboard.instantiateInitialViewController() as! ViewController
+        
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    // test -> percentage
+    func testPercentageCalculator() {
+        // Should be 25
+        let p = vc.percentage(50, 50)
+        XCTAssert(p == 25)
+    }
+    
+    // test numberLabel、percentageLabel、resultLabel
+    func testLabelValuesShowedProperly() {
+        
+        // fix label is nil bug
+        let _ = vc.view
+        
+        vc.updateLabels(Float(80.0), Float(50.0), Float(40.0))
+        
+        // The labels should now display 80, 50 and 40
+        XCTAssert(vc.numberLabel.text == "80.0", "numberLabel doesn't show the right text")
+        XCTAssert(vc.percentageLabel.text == "50.0%", "percentageLabel doesn't show the right text")
+        XCTAssert(vc.resultLabel.text == "40.0", "resultLabel doesn't show the right text")
+    }
+    
+}
