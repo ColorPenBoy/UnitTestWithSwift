e1453163af4731a98c72ec76fb4ccbf4aeebe813
张强 <zq252125@163.com>
2017-03-05 16:13:10 +0800

    convert to swift 3.0

--- UnitTestStarted/PercentageCalculator/ViewController.swift
+++ UnitTestStarted/PercentageCalculator/ViewController.swift
@@ -22,11 +22,9 @@ class ViewController: UIViewController {
     // The lowest label, showing the percentage result.
     @IBOutlet weak var resultLabel: UILabel!
     
-    
-    
     // MARK: - Methods for UIElements
     // Called when the numberSlider value is changed. It will update its value to be a whole number and calculate and display the percentage results.
-    @IBAction func numberValueChanged(sender: AnyObject) {
+    @IBAction func numberValueChanged(_ sender: AnyObject) {
         numberSlider.setValue(Float(Int(numberSlider.value)), animated: true)
         
         let r = percentage(numberSlider.value, percentageSlider.value)
@@ -34,7 +32,7 @@ class ViewController: UIViewController {
     }
     
     // Called when the percentageSlider value is changed. It will update its value to be a whole number and calculate and display the percentage results.
-    @IBAction func percentageValueChanged(sender: AnyObject) {
+    @IBAction func percentageValueChanged(_ sender: AnyObject) {
         percentageSlider.setValue(Float(Int(percentageSlider.value)), animated: true)
         
         let r = percentage(numberSlider.value, percentageSlider.value)
@@ -42,7 +40,7 @@ class ViewController: UIViewController {
     }
     
     // This will update the all the labels when one of the two sliders values has been changed.
-    func updateLabels(nV: Float?, _ pV: Float?, _ rV: Float) {
+    func updateLabels(_ nV: Float?, _ pV: Float?, _ rV: Float) {
         if let v = nV {
             self.numberLabel.text = "\(v)"
         }
@@ -60,11 +58,9 @@ class ViewController: UIViewController {
     
     // MARK: - Percentage Calculation
     // This calculates the percentage result of two given numbers.
-    func percentage(value: Float, _ percentage: Float) -> Float {
+    func percentage(_ value: Float, _ percentage: Float) -> Float {
         return value * (percentage / 100)
     }
-    
-    
     
     // MARK: - System methods
     override func viewDidLoad() {
